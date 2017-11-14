d3792ab8107c887287e44ab516a2b4a6b2cf4f06
Christian Stocker <me@chregu.tv>
2017-11-14 08:33:33 +0100

    saveHash expects a SourceImage

--- rokkacallbacks.php
+++ rokkacallbacks.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use Rokka\Client\Core\SourceImage;
 use Rokka\Client\LocalImage\LocalImageAbstract;
 use Rokka\Client\TemplateHelperCallbacksAbstract;
 
@@ -10,9 +11,9 @@ class rokkacallbacks extends TemplateHelperCallbacksAbstract {
     return Rokka::getRokkaHash($image->getContext());
   }
 
-  public function saveHash(LocalImageAbstract $file, $hash, $shortHash) {
-    $file->getContext()->update([rokka::getRokkaHashKey() => $shortHash], rokka::DEFAULT_TXT_LANG);
-    return $shortHash;
+  public function saveHash(LocalImageAbstract $file, SourceImage $sourceImage) {
+    $file->getContext()->update([rokka::getRokkaHashKey() => $sourceImage->shortHash], rokka::DEFAULT_TXT_LANG);
+    return $sourceImage->shortHash;
   }
   public function getMetadata(LocalImageAbstract $image): array {
     return ['meta_user' => ['kirby_location_on_upload' => dirname(parse_url($image->getContext()->url(), PHP_URL_PATH))]];
