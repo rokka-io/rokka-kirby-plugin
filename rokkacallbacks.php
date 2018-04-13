8db5a0813d07a81dda981c6ec05189d1af88023e
Christian Stocker <me@chregu.tv>
2018-04-13 14:20:48 +0200

    use renamed callback and localimage classes

--- rokkacallbacks.php
+++ rokkacallbacks.php
@@ -2,20 +2,20 @@
 
 
 use Rokka\Client\Core\SourceImage;
-use Rokka\Client\LocalImage\LocalImageAbstract;
-use Rokka\Client\TemplateHelperCallbacksAbstract;
+use Rokka\Client\LocalImage\AbstractLocalImage;
+use Rokka\Client\TemplateHelper\AbstractCallbacks;
 
-class rokkacallbacks extends TemplateHelperCallbacksAbstract {
+class rokkacallbacks extends AbstractCallbacks {
 
-  public function getHash(LocalImageAbstract $image) {
+  public function getHash(AbstractLocalImage $image) {
     return Rokka::getRokkaHash($image->getContext());
   }
 
-  public function saveHash(LocalImageAbstract $file, SourceImage $sourceImage) {
+  public function saveHash(AbstractLocalImage $file, SourceImage $sourceImage) {
     $file->getContext()->update([rokka::getRokkaHashKey() => $sourceImage->shortHash], rokka::DEFAULT_TXT_LANG);
     return $sourceImage->shortHash;
   }
-  public function getMetadata(LocalImageAbstract $image): array {
+  public function getMetadata(AbstractLocalImage $image): array {
     return ['meta_user' => ['kirby_location_on_upload' => dirname(parse_url($image->getContext()->url(), PHP_URL_PATH))]];
   }
 }
